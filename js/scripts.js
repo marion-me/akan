1170c536d5d2ca8a92fb25da99e076d7249533e1
marion vutagwa <marionvutagwa@yahoo.com>
2020-02-16 20:59:09 +0300

    Add js file CHANGES

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 var day = d
-ar modulus = function (d) = (((cc / 4) - 2 * cc - 1) + ((5 * yy / 4) + ((26 * (mm + 1)/10 +dd) modulus 7 {
+ar modulus = function (d) = (((CC / 4) - 2 * cc - 1) + ((5 * YY / 4) + ((26 * (MM + 1)/10 +DD) modulus 7 {
     return (((cc / 4) - 2 * cc - 1) + ((5 * yy / 4) + ((26 * (mm + 1)/10 +dd) %7
 }
+function d ()
 var date = document.getElementById("date").value;
 var month = document.getElementById("month").value;
 var year = document.getElementById("year").value;
@@ -12,16 +13,16 @@ var day = d.getDay();
 if (day === 0) {
     return "Sunday";
   } else if (day === 1) {
-    return "Monday";
+     return "Monday";
   } else if (day === 2) {
-        return "Tuesday"; 
- }  else if (day === 3) {
+     return "Tuesday"; 
+  } else if (day === 3) {
      return "Wednesday";
- }  else if (day === 4) {
+  } else if (day === 4) {
      return "Thursday";
- }  else if (day === 5) { 
+  } else if (day === 5) { 
      return "Friday";
- }  else if (day === 4) {
+  } else if (day === 4) {
      return "Saturday";
   
 
